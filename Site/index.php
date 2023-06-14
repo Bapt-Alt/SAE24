da57c71a07cddd4d4d9471367f62f245e0994806
Bapt-Alt <128176837+Bapt-Alt@users.noreply.github.com>
2023-06-14 17:02:05 +0200

    Création index + css
    Début de la création de la navbar

---
+++ Site/index.php
@@ -0,0 +1,31 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS for the page-->
+    <link rel="stylesheet" href="./Style/style.css">
+    <link rel="icon" href="" type="image/gif">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <span class="logo">SAE 24</span>
+            <ul class="pages">
+                <li><a class="" href="">Accueil</a></li>
+                <li><a class="" href="">Présentation du cabinet</a></li>
+                <li><a class="" href="">Notre équipe</a></li>
+                <li><a class="" href="">(Prise de RDV)</a></li>
+                <li><a class="" href="">Mention légale</a></li>
+                <li><a class="" href="">Ou nous trouver</a></li>
+            </ul>
+            <a class="connexion_btn" href="">Connexion</a>
+        </nav> 
+    </header>    
+</body>
+</html>
