e79c7005c4571e8eaeb924ba3623307836600a8a
Bapt-Alt <128176837+Bapt-Alt@users.noreply.github.com>
2023-06-15 20:55:02 +0200

    Ajout Accueil + Cabinet
    Création de la section accueil + cabinet et le css qui va avec.
    Ajout de l'effet swip pour passer d'une section à l'autre

--- Site/index.php
+++ Site/index.php
@@ -17,15 +17,15 @@
         <nav>
             <img class="logo" src="./Image/LogoVert.png" alt="Logo">
             <ul>
-                <li><a class="" href="">Accueil</a></li>
+                <li><a class="swipe" href="#Accueil">Accueil</a></li>
                 <li class="responsive bull"><p>&bull;</p></li>
-                <li><a class="" href="">cabinet</a></li>
+                <li><a class="swipe" href="#Cabinet">Cabinet</a></li>
                 <li class="responsive bull"><p>&bull;</p></li>
-                <li><a class="" href="">Notre équipe</a></li>
+                <li><a class="swipe" href="">Notre équipe</a></li>
                 <li class="responsive bull"><p>&bull;</p></li>
-                <li><a class="" href="">(Prise de RDV)</a></li>
+                <li><a class="swipe" href="">(Prise de RDV)</a></li>
                 <li class="responsive bull"><p>&bull;</p></li>
-                <li><a class="" href="">Ou nous trouver</a></li>
+                <li><a class="swipe" href="">Ou nous trouver</a></li>
                 <li class="responsive bull"><p>&bull;</p></li>
                 <a class="connexion_btn responsive" href="">Connexion</a>
             </ul>
@@ -34,10 +34,26 @@
             <label for="menu-toggle" class="menu-icon">&#9776;</label>
         </nav> 
     </header>
-    <section id="">
-
+    <section id="Accueil">
+        <ul>
+            <li>
+                <h1><span>Meilleurs</span> soins, <br>
+                    <span>Meilleurs</span> docteurs</h1>
+                <h2>Votre bien être et votre santé sont notre priorité</h2>
+            </li>
+            <li><img src="./Image/accueil-removebg.png" alt="Un docteur qui soigne un patient"></li>
+        </ul>
     </section>
-    <section id=""></section>
+    <section id="Cabinet">
+        <ul>
+            <li><img src="./Image/cabinet-removebg.png" alt="Un cabinet"></li>
+            <li>
+                <h1>Notre <span>Cabinet</span>,</h1>
+                <p><span>&bull;</span> Nos experts en médecine mettent tout en oeuvre pour vous fournir les meilleurs soins dans les meilleurs delais. </p>
+                <p><span>&bull;</span> Nous sommes équipé d'équipement à la pointe de la technologie nous permettant d'éxercer une médecine présice et efficace.</p>
+            </li>
+        </ul>
+    </section>
     <section id=""></section>
     <section id=""></section>
     <section id=""></section>
@@ -71,7 +87,32 @@
             }
         }
         });
-
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        // Écouteur d'événement pour tous les liens de navigation
+        var navLinks = document.querySelectorAll('a.swipe');
+        navLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Empêche le comportement de lien par défaut
+            
+            // Récupère l'ID de la section cible depuis l'attribut href du lien
+            var targetId = link.getAttribute('href');
+            
+            // Trouve la section cible dans le DOM
+            var targetSection = document.querySelector(targetId);
+            
+            // Vérifie si la section cible existe et défile jusqu'à elle en douceur
+            if (targetSection) {
+            var targetOffsetTop = targetSection.offsetTop;
+            window.scrollTo({
+                top: targetOffsetTop,
+                behavior: 'smooth'
+            });
+            }
+        });
+        });
+  });
     </script>
 </body>
 </html>
