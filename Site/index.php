6f4734942251cf4181e8dd945f832d1f3ae84a9c
Bapt-Alt <128176837+Bapt-Alt@users.noreply.github.com>
2023-06-15 13:04:49 +0200

    Fin de la navbar responsive

--- Site/index.php
+++ Site/index.php
@@ -18,10 +18,16 @@
             <img class="logo" src="./Image/LogoVert.png" alt="Logo">
             <ul>
                 <li><a class="" href="">Accueil</a></li>
+                <li class="responsive bull"><p>&bull;</p></li>
                 <li><a class="" href="">cabinet</a></li>
+                <li class="responsive bull"><p>&bull;</p></li>
                 <li><a class="" href="">Notre équipe</a></li>
+                <li class="responsive bull"><p>&bull;</p></li>
                 <li><a class="" href="">(Prise de RDV)</a></li>
+                <li class="responsive bull"><p>&bull;</p></li>
                 <li><a class="" href="">Ou nous trouver</a></li>
+                <li class="responsive bull"><p>&bull;</p></li>
+                <a class="connexion_btn responsive" href="">Connexion</a>
             </ul>
             <a class="connexion_btn" href="">Connexion</a>
             <input type="checkbox" id="menu-toggle" class="menu-toggle">
@@ -42,15 +48,30 @@
     </footer>
     <script>
         window.addEventListener('scroll', function() {
-  var scrollPosition = window.scrollY;
-  var element = document.querySelector('.scroll-shadow');
-  
-  if (scrollPosition > 0) {
-    element.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.2)';
-  } else {
-    element.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0)';
-  }
-});
-    </script>   
+            var scrollPosition = window.scrollY;
+            var element = document.querySelector('.scroll-shadow');
+            
+            if (scrollPosition > 0) {
+                element.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.2)';
+            } else {
+                element.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0)';
+            }
+        });
+    </script>
+    <script>
+        const menuToggle = document.getElementById('menu-toggle');
+        const navUl = document.querySelector('nav ul');
+
+        menuToggle.addEventListener('click', function() {
+        if (window.innerWidth <= 1200) {
+            if (navUl.classList.contains('active')) {
+            navUl.classList.remove('active');
+            } else {
+            navUl.classList.add('active');
+            }
+        }
+        });
+
+    </script>
 </body>
 </html>
